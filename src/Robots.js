c730b28eaa428863e7e63b1370882d99eb1e35d6
Phan Anh Tuan <phananhtuan1011@gmail.com>
2019-04-29 14:10:42 +0700

    Push to github

---
+++ src/Robots.js
@@ -0,0 +1,44 @@
+export const robots = [ 
+	{
+		id: 1,
+		name: 'Leanne Graham',
+		username: 'Bret', 
+		email: 'abc@gmail.com',
+	},
+	{
+		id: 2,
+		name: 'Tuan Phan',
+		username: 'soloism', 
+		email: 'tuan@gmail.com',
+	},
+	{
+		id: 3,
+		name: 'Bu Gia Map',
+		username: 'bugiamap', 
+		email: 'abc@gmail.com',
+	},
+	{
+		id: 4,
+		name: 'Ta Nang',
+		username: 'tanang', 
+		email: 'tanang@gmail.com',
+	},
+	{
+		id: 5,
+		name: 'Phan Xi Pang',
+		username: 'phanxipang', 
+		email: 'phanxipang@gmail.com',
+	},
+	{
+		id: 6,
+		name: 'Nepal ABC',
+		username: 'nepalabc', 
+		email: 'nepal@gmail.com',
+	},
+	{
+		id: 7,
+		name: 'Thailand',
+		username: 'thailand', 
+		email: 'thailand@gmail.com',
+	},
+]
